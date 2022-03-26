7c22b91e76602379d11fedb5e98e71a31b39919a
alaa afifi <alaaahmedafifi@gmail.com>
2022-03-26 08:47:32 +0200

    contactme form validation

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-contactme',
-  templateUrl: './contactme.component.html',
-  styleUrls: ['./contactme.component.css']
+	selector: 'app-contactme',
+	templateUrl: './contactme.component.html',
+	styleUrls: ['./contactme.component.css']
 })
 export class ContactmeComponent implements OnInit {
 
-  constructor() { }
+	contactForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor() { 
+		this.contactForm = new FormGroup({
+			name: new FormControl('', {
+				validators: [
+					Validators.required,
+					Validators.minLength(3),
+				],
+				updateOn: "blur"
+			}),
+			email: new FormControl('', {
+				validators: [
+					Validators.required,
+					Validators.minLength(8),
+					Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
+				],
+				updateOn: "blur"
+			}),
+			message: new FormControl('', {
+				validators: [
+					Validators.required
+				]
+			})
+		})
+	}
+
+	ngOnInit(): void {
+	}
+
+	get name(){
+		return this.contactForm.get('name');
+	}
+
+	get email(){
+		return this.contactForm.get('email');
+	}
+
+	get message(){
+		return this.contactForm.get('message');
+	}
+
+	onContactSubmit(){
+		let data = {
+			name: this.contactForm.value.name,
+			email: this.contactForm.value.email,
+			message: this.contactForm.value.message
+		}
+
+	}
 
 }
